24112c42468f3a0309e08cfbd711e278963a3a2b
minischetti <ZfLsL_Kn7j_aQws_R84P>
2021-07-12 18:29:06 -0700

    Improve styles

---
+++ components/ui/Hero.js
@@ -0,0 +1,8 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+    return (
+        <div className={styles.hero}>
+        </div>
+    )
+}
--- pages/music.js
+++ pages/music.js
@@ -6,19 +6,52 @@ export default function MusicHome() {
     return (
         <BasePage>
             <div className={styles.hero}>
-            </div>
-            <div>
-                <div className={styles.section}>
-                    <div className={styles.image}>
-                        <Image width="250" height="250" src="/music/music-bio-hero.jpeg"/>
+                <div className={styles.imagesContainer}>
+                    <div className={styles.images}>
+                        <div className={styles.imageContainer}>
+                            <Image src={"/music/music-0.jpg"}
+                                   layout='fill'
+                                   objectFit='contain'>
+                            </Image>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <Image src={"/music/music-1.jpg"}
+                                   layout='fill'
+                                   objectFit='contain'>
+                            </Image>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <Image src={"/music/music-2.jpg"}
+                                   layout='fill'
+                                   objectFit='contain'>
+
+                            </Image>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <Image src={"/music/music-3.jpg"}
+                                   layout='fill'
+                                   objectFit='contain'>
+
+                            </Image>
+                        </div>
                     </div>
-                    <div>
+                </div>
+            </div>
+            <div className={styles.sections}>
+                <div className={styles.section}>
+                    <div className={styles.imageContainer}>
+                        <Image src="/music/music-bio-hero.jpeg" width="250" height="250" layout={"responsive"}/>
+                    </div>
+                    <div className={styles.sectionContent}>
                         <h1>Biography</h1>
                         <p>I have over 30 years of professional playing experience and over 20 years of teaching experience with a B.A. in Music from DePaul University and a M.M. in Guitar Performance from Cleveland State University. I have led my own solo music projects as well as recorded with various rock, blues, jazz and hip hop artists.</p>
                     </div>
                 </div>
                 <div className={styles.section}>
-                    <div>
+                    <div className={styles.imageContainer}>
+                        <Image src="/music/music-lessons-hero.jpeg" width="250" height="250" layout={"responsive"}/>
+                    </div>
+                    <div className={styles.sectionContent}>
                         <h1>Lessons</h1>
                         <p>Lessons are custom designed to challenge the student to consistently improve and achieve their goals. Students must have Skype or some type of video chat. After submitting a secured payment through PayPal you will receive an email to schedule your online lesson.</p>
                         <div className={styles.buttons}>
@@ -34,9 +67,6 @@ export default function MusicHome() {
                                 </div>
                             </a>
                         </div>
-                    </div>
-                    <div className={styles.image}>
-                        <Image width="250" height="250" src="/music/music-lessons-hero.jpeg"/>
                     </div>
                 </div>
             </div>
