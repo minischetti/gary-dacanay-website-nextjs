c5454e0f8a60986ac02596660a75927bf8a80ee7
minischetti <ZfLsL_Kn7j_aQws_R84P>
2021-07-12 20:27:32 -0700

    Add yoga page
    * Add basic yoga page
    * Update hero image styles

--- pages/yoga.js
+++ pages/yoga.js
@@ -1,18 +1,16 @@
 import BasePage, {THEME} from "@/components/BasePage";
 import styles from "./yoga.module.css";
 import Image from "next/image";
+import HeroImage from "@/components/ui/HeroImage";
+import Section from "@/components/ui/Section";
 
 export default function Yoga() {
     return (
         <BasePage theme={THEME.LIGHT}>
-            <div className={styles.hero}>
-            </div>
-            <div>
+            <HeroImage imageSrc={"/yoga/yoga-background.jpg"}/>
+            <Section imageSrc="/yoga/yoga-hero.jpeg">
                 <p>I am a 500 Hour Certified <a href="https://www.baptisteyoga.com/trainer-profile/?user=1910" target="_blank" rel={"noreferrer"}>Baptiste Yoga</a> Instructor, serving the Cleveland community at Cleveland Public Schools and family recreation centers. I also teach in corporate settings, lead workshops and master classes. and facilitate private lessons.</p>
-                <div className={styles.image}>
-                    <Image width="250" height="250" src="/yoga/yoga-hero.jpeg"></Image>
-                </div>
-            </div>
+            </Section>
         </BasePage>
     )
 }
