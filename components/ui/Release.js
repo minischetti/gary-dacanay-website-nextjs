ae617c6ad6a1e3bf0baee57f0277d5e4fb2a4c57
minischetti <ZfLsL_Kn7j_aQws_R84P>
2021-07-20 17:10:19 -0700

    Fix homepage layout

--- components/ui/Release.js
+++ components/ui/Release.js
@@ -5,6 +5,9 @@ import Img from "@/components/ui/Img";
 export default function Release({imageSrc, title, description, spotifyUrl, appleMusicUrl}) {
     return (
         <div className={styles.release}>
+            <div className={styles.image}>
+                <Img width="500" height="500" src={imageSrc} layout={"responsive"}></Img>
+            </div>
             <div className={styles.releaseContent}>
                 <div>
                     <div className={styles.title}>{title}</div>
@@ -15,9 +18,7 @@ export default function Release({imageSrc, title, description, spotifyUrl, apple
                     <a href={appleMusicUrl} target={"_blank"} rel={"noreferrer"} className={styles.button}><SiApple/>Apple Music</a>
                 </div>
             </div>
-            <div className={styles.image}>
-                <Img width="500" height="500" src={imageSrc} layout={"responsive"}></Img>
-            </div>
+
         </div>
     )
 }
