330840e618488fb06ef81f8563cc928fd4507185
minischetti <ZfLsL_Kn7j_aQws_R84P>
2021-07-22 19:58:26 -0700

    Improve mobile support

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width viewport-fit=cover" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+  ) 
 }
 
 export default MyApp
