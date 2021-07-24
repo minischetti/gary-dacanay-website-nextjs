9960d1c8c6badb56ffd382cf09621daf9a1a40ae
minischetti <ZfLsL_Kn7j_aQws_R84P>
2021-07-23 17:42:53 -0700

    Yoga image autoloader and refinements

--- pages/music.js
+++ pages/music.js
@@ -4,13 +4,12 @@ import HeroImage from "@/components/ui/HeroImage";
 import Gallery from "@/components/ui/Gallery";
 import musicBackground from "../public/music/music-background.jpg";
 import music0 from "../public/music/music-0.jpg";
+import music1 from "../public/music/music-1.jpg";
 import music2 from "../public/music/music-2.jpg";
-import music3 from "../public/music/music-3.jpg";
 
 import React from "react";
 import MusicPage from "@/components/view/MusicPage";
 import Venmo from "@/components/ui/Venmo";
-import SlideIn from "@/components/ui/SlideIn";
 import Divider from "@/components/ui/Divider";
 
 export default function Music() {
@@ -19,7 +18,7 @@ export default function Music() {
             <HeroImage imageSrc={musicBackground}/>
             <div className= {styles.sections}>
                 <Section title="Biography">
-                    <p>I have over 30 years of professional playing experience and over 20 years of teaching experience with a B.A. in Music from DePaul University and a M.M. in Guitar Performance from Cleveland State University. I have led my own solo music projects as well as recorded with various rock, blues, jazz and hip hop artists.</p>
+                    <p>I have over 30 years of professional playing experience and over 20 years of teaching experience with a B.A. in Music from DePaul University and a M.M. in Guitar Performance from Cleveland State University. I lead my own solo music projects as well as record with various rock, blues, jazz and hip hop artists.</p>
                 </Section>
                 <Section title="Lessons">
                     <p>Lessons are custom designed to challenge the student to consistently improve and achieve their goals. Students must have Skype or some type of video chat. After submitting a secured payment through Venmo you will receive an email to schedule your online lesson.</p>
@@ -27,7 +26,7 @@ export default function Music() {
                     <Venmo tag="guitarfire"/>
                 </Section>
             </div>
-            <Gallery imageSrcs={[music0, music3, music2]}/>
+            <Gallery imageSrcs={[music0, music1, music2]}/>
         </MusicPage>
     )
 }
--- pages/yoga.js
+++ pages/yoga.js
@@ -1,11 +1,25 @@
 import HeroImage from "@/components/ui/HeroImage";
 import Section from "@/components/ui/Section";
 
-import yoga1 from "../public/yoga/yoga-1.jpeg";
-import yoga2 from "../public/yoga/yoga-2.jpeg";
-import yoga3 from "../public/yoga/yoga-3.jpeg";
+// import yoga1 from "../public/yoga/yoga-1.jpeg";
+// import yoga2 from "../public/yoga/yoga-2.jpeg";
+// const {test} = await import("../public/yoga/gallery/yoga-3.jpeg").then(module => module);
+// console.log(test);
 import Gallery from "@/components/ui/Gallery";
 import {Link} from "@/components/ui/Link";
+
+const getFiles = (directory) => {
+    const filesContext = require.context("public/yoga/gallery", false, /.*jpeg/);
+    console.log(filesContext);
+    const fileKeys = filesContext.keys();
+    const modules = fileKeys.map(key => filesContext(key).default);
+    const deDupedModules = [... new Set(modules)];
+    return deDupedModules;
+}
+
+const yogaPictures = getFiles("public/yoga/gallery");
+console.log(yogaPictures);
+
 
 import yogaBackground from "../public/yoga/yoga-background.jpg"
 import YogaPage from "@/components/view/YogaPage";
@@ -18,7 +32,7 @@ export default function Yoga() {
                 <p>I am a 500 Hour Certified Baptiste Yoga Instructor, serving the Cleveland community at Cleveland Public Schools and family recreation centers. I also teach in corporate settings, lead workshops and master classes, and facilitate private lessons.</p>
                 <Link href={"https://www.baptisteyoga.com/trainer-profile/?user=1910"}>Learn more</Link>
             </Section>
-            <Gallery imageSrcs={[yoga1, yoga3, yoga2]}/>
+            <Gallery imageSrcs={yogaPictures}/>
         </YogaPage>
     )
 }
