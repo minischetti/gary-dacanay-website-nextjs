33ba46aa68ef11bc100febb03d9f8721164cba79
Dominic Minischetti <dominic_minischetti@anfcorp.com>
2023-08-05 12:11:08 -0700

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
 
 class MyDocument extends Document {
     render() {
@@ -15,6 +16,7 @@ class MyDocument extends Document {
                 <body className="font-sans tracking-wide">
                     <Main />
                     <NextScript />
+                    <Analytics />
                 </body>
             </Html>
         )
