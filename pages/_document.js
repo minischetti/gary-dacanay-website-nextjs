30b107b5860b3fb0792fad14652e4d7c4ed519ab
Dominic Minischetti <dominic_minischetti@anfcorp.com>
2023-08-05 12:14:52 -0700

    Remove unused Google font imports

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
                     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js" crossOrigin={"true"}></script>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Lato:ital@0;1&family=Roboto:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&family=Source+Sans+Pro:ital@0;1&display=swap"
                         rel="stylesheet"/>
                 </Head>
                 <body className="font-sans tracking-wide">
